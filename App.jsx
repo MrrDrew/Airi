c9ae37425eadd00e4e5467929dd667d29c17bf12
MrrDrew <mrdrew@riseup.net>
2026-03-14 14:52:28 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -122,6 +122,12 @@ const allTimezones =
 
 
 
+const params = new URLSearchParams(window.location.search);
+const initialScreen = params.get("screen") === "timezone" ? "timezone" : "calendar";
+const [screen, setScreen] = useState(initialScreen);
+
+
+
 function getTimezoneOffsetLabel(timeZone) {
   try {
     const now = new Date();
